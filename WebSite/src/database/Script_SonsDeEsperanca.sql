8f2403e73278095a4f3ae3806c7f38832e6c043b
RebecaCagni <rebeca.cagni@sptech.school>
2024-06-05 20:49:43 -0300

    Adicionando ~sessão de publicações, alteração nos textos e css

--- WebSite/src/database/Script_SonsDeEsperanca.sql
+++ WebSite/src/database/Script_SonsDeEsperanca.sql
@@ -9,16 +9,16 @@ email varchar(50),
 senha varchar(50)
 );
 
-
 create table questionario (
-idInstrumento int auto_increment,
+idQuestionario int auto_increment,
 fkUsuario int,
 constraint FKUsuarioQuestionario foreign key (fkUsuario)
 references usuario(id),
-constraint pkComposta primary key (idInstrumento, fkUsuario),
+constraint pkComposta primary key (idQuestionario, fkUsuario),
 pontuacao varchar(10),
 nomeInstrumento varchar(40)
 );
+
  
 create table aula (
 idAula int primary key auto_increment,
@@ -28,9 +28,26 @@ fkUsuario int,
 constraint fk foreign key (fkUsuario)
 references usuario(id)
 );
-select * from questionario;
 
-select count(NomeInstrumento) from questionario 
-group by nomeInstrumento;
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+	descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+);
+
+
+select * from questionario order by nomeInstrumento;
+
+select count(nomeInstrumento) as quantidade from questionario 
+group by nomeInstrumento order by nomeInstrumento;
+
+SELECT nomeInstrumento 
+        FROM questionario;
 
 desc instrumento;
+
+ SELECT nomeInstrumento, COUNT(*) as quantidade
+        FROM instrumento
+        GROUP BY '${resposta}';
